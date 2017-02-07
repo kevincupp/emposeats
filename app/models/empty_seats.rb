770f67489439adb2b3a9cbc04ef5247b6a4a8ec1
Kevin Cupp <kevin.cupp@gmail.com>
2017-02-06 22:13:20 -0500

    EmptySeats model saves date automatically

--- app/models/empty_seats.rb
+++ app/models/empty_seats.rb
@@ -1,3 +1,9 @@
 class EmptySeats < ActiveRecord::Base
   self.table_name = 'emptyseats'
+
+  before_save :default_values
+  after_create :broadcast_cable
+  def default_values
+    self.date ||= Time.new.strftime('%a, %d %b %Y %H:%M:%S %z')
+  end
 end
