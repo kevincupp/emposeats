68e6efca10ba53fb5f42ae6174971b35642c9e83
Kevin Cupp <kevin.cupp@gmail.com>
2017-02-11 19:15:08 -0500

    Fix whitespace in emposeats controller

--- app/controllers/emposeats_controller.rb
+++ app/controllers/emposeats_controller.rb
@@ -1,16 +1,16 @@
 class EmposeatsController < ApplicationController
-	def index
-		@quote = BTTFQuote.order("RAND()").first.quote
-		@dailystats = DailyStat.order(id: :desc).limit(15)
-		@seats = EmptySeats.order(id: :desc).first.seats
-		@currentstats = CurrentStats.first
-	end
+  def index
+    @quote = BTTFQuote.order("RAND()").first.quote
+    @dailystats = DailyStat.order(id: :desc).limit(15)
+    @seats = EmptySeats.order(id: :desc).first.seats
+    @currentstats = CurrentStats.first
+  end
 
-	def self.broadcast_new_seats
-		seats = EmptySeats.order(id: :desc).first.seats
-		currentstats = CurrentStats.first
-		markup = render partial: 'emposeats/seats', locals: {seats: seats, currentstats: currentstats }
+  def self.broadcast_new_seats
+    seats = EmptySeats.order(id: :desc).first.seats
+    currentstats = CurrentStats.first
+    markup = render partial: 'emposeats/seats', locals: {seats: seats, currentstats: currentstats }
 
-		ActionCable.server.broadcast 'seats', markup: markup
-	end
+    ActionCable.server.broadcast 'seats', markup: markup
+  end
 end
