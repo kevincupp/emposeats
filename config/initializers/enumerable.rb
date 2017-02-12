63ce4921e7fe04fa87f2b44cf7e747644d5ace76
Kevin Cupp <kevin.cupp@gmail.com>
2017-02-11 22:20:47 -0500

    Fix whitespace in enumerable mixin

--- config/initializers/enumerable.rb
+++ config/initializers/enumerable.rb
@@ -1,18 +1,18 @@
 module Enumerable
-	def average
-		self.sum / self.size
-	end
+  def average
+    self.sum / self.size
+  end
 
-	def std_dev
-		average = self.average
-		std_dev = 0
+  def std_dev
+    average = self.average
+    std_dev = 0
 
-		self.each do |n|
-			n -= average
-			n **= 2
-			std_dev += n
-		end
+    self.each do |n|
+      n -= average
+      n **= 2
+      std_dev += n
+    end
 
-		Math.sqrt(std_dev / self.size).round
-	end
+    Math.sqrt(std_dev / self.size).round
+  end
 end
